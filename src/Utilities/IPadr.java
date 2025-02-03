205d18e75c0cb13f4ace2d6a976cf1f921057d24
zachws <95520780+zachws@users.noreply.github.com>
2025-02-02 21:02:13 -0700

    Update IPadr.java
    Formatting.

--- src/Utilities/IPadr.java
+++ src/Utilities/IPadr.java
@@ -1,7 +1,7 @@
 package Utilities;
 
 public class IPadr {
-    public static final String VIC_IP = "127.0.01;
+    public static final String VIC_IP = "127.0.01";
     public static final String OWEN_IP = "127.0.01";
     public static final String DERRICK_IP = "127.0.01";
     public static final String ZACH_IP = "127.0.01";
