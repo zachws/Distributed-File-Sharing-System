95712efbb5ca079f0aa313356f9a0f14a31b7626
zachws <95520780+zachws@users.noreply.github.com>
2025-02-02 21:01:51 -0700

    Update IPadr.java
    Accidental dox.

--- src/Utilities/IPadr.java
+++ src/Utilities/IPadr.java
@@ -1,9 +1,9 @@
 package Utilities;
 
 public class IPadr {
-    public static final String VIC_IP = "192.168.117.250";
-    public static final String OWEN_IP = "192.168.117.90";
-    public static final String DERRICK_IP = "192.168.117.86";
-    public static final String ZACH_IP = "192.168.117.25";
-    public static final String MATT_IP = "192.168.117.49";
+    public static final String VIC_IP = "127.0.01;
+    public static final String OWEN_IP = "127.0.01";
+    public static final String DERRICK_IP = "127.0.01";
+    public static final String ZACH_IP = "127.0.01";
+    public static final String MATT_IP = "127.0.01";
 }
